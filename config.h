a7743f881cc1bc44f64e0c9aab8e54bfc91a2c60
Jason Pena <jasonpena@awkless.com>
2023-12-07 14:44:58 -0700

    feat: Apply restart sig patch
    https://dwm.suckless.org/patches/restartsig/
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.h
+++ config.h
@@ -5,17 +5,19 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = {
+	"Hurmit Nerd Font:style=Regular:pixelsize=16"
+};
+
+static const char fg_norm[]      = "#fce8c3";
+static const char fg_sel[]       = "#f75341";
+static const char bg_norm[]      = "#1c1b19";
+static const char border_norm[]  = "#918175";
+static const char border_sel[]   = "#EF2F27";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg       bg          border   */
+	[SchemeNorm] = { fg_norm, bg_norm,    border_norm },
+	[SchemeSel]  = { fg_sel,  border_sel, border_sel },
 };
 
 /* tagging */
@@ -56,8 +58,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0";
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
@@ -95,7 +97,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
 /* button definitions */
--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # dwm version
-VERSION = 6.4
+VERSION = 6.4.0-patch.1
 
 # Customize below to fit your system
 
